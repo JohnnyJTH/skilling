1f4a63a9a820dd69ca6803388a22d50b5731adf4
Johnny JTH <johanohly@gmail.com>
2024-04-29 14:46:14 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card';
+</script>
+
+<div class="flex">
+	<Card.Root>
+		<Card.Header>
+			<Card.Description>Fysisk fravær, for året</Card.Description>
+			<Card.Title class="text-4xl">IDK</Card.Title>
+		</Card.Header>
+		<Card.Content>
+			<div class="text-xs text-muted-foreground">idk dik</div>
+		</Card.Content>
+	</Card.Root>
+</div>
